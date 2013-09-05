f2d58e3ae20cb04c71c72e5003955db166db26a3
Richard Vuduc <richie@cc.gatech.edu>
2013-09-05 01:02:11 -0400

    Initial version

---
+++ code/parallel-mergesort--omp.cc
@@ -0,0 +1,19 @@
+/**
+ *  \file parallel-mergesort--omp.cc
+ *
+ *  \brief Implement your parallel mergesort in this file.
+ */
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sort.hh"
+
+void
+parallelSort (int N, keytype* A)
+{
+  /* Lucky you, you get to start from scratch */
+}
+
+/* eof */
