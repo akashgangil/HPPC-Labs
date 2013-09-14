f512d5b47defa6f4107d846db47c1be3e46cb333
Akash Gangil <akash.gangil@outlook.com>
2013-09-14 11:29:19 -0400

    code cleanup

--- code/parallel-mergesort--omp.cc
+++ code/parallel-mergesort--omp.cc
@@ -5,9 +5,9 @@
  */
 
 #include <assert.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "sort.hh"
 //#define DEBUG 
@@ -28,10 +28,6 @@ keytype* merge(keytype *left, keytype *right, int start, int end, int mid)
 
 keytype* mergeSort(int start, int end, keytype* a)
 {
-#ifdef DEBUG		
-  printf("**********************");
-  printf("\nINSIDE MERGE SORT\n");
-#endif
   int N = end - start;
   if(N <= 100)
   {
@@ -39,36 +35,14 @@ keytype* mergeSort(int start, int end, keytype* a)
     return a;
   }
   
-  if(N == 1)
-  {
-#ifdef DEBUG
-    printf("SINGLE ELEMENT\n");
-    printf("ELEMENT: %d\n", a[start]);
-#endif				
-    return a;
-  }
-  
   int mid = (end - start)/2;
 
-#ifdef DEBUG
-  printf("Start: %3d  End: %3d\n", start, end);
-  printf("N : %3d  mid: %3d\n", N, mid);
-#endif
   keytype* left = mergeSort(start, start+mid, a);
   keytype* right = mergeSort(start+mid, end, a);
-#ifdef DEBUG
-  printf("LEFT ARRAY\n");
-  display(a, 0, 8);
-  printf("RIGHT ARRAY\n");
-  display(a, 0, 8);
-
-  printf("\n");
-  printf("MERGING!!!!!!! with params start %3d end %3d mid %3d N %3d\n", start, end, mid, N);
-#endif
+  
   keytype *newArr = merge(left, right, start, end, mid);
   memcpy(a+start, newArr, N*sizeof(int));
   free(newArr);
-  display(a, 0, 8);
   return a;
 }
 
