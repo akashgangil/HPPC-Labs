fe1f4a613eb2297a0c7a76c147f2d4ba3d5278b5
Akash Gangil <akash.gangil@outlook.com>
2013-09-14 15:39:21 -0400

    minor changes and cleanup

--- code/parallel-mergesort--omp.cc
+++ code/parallel-mergesort--omp.cc
@@ -12,9 +12,13 @@
 #include "sort.hh"
 //#define DEBUG 
 
+
+/*Displays the Array*/
 void display(keytype* a, int start, int N)
 {
+#ifdef DEBUG
   printf("INSIDE DISPLAY : VALUES start %3d N %3d\n", start, N);
+#endif
   while(N--)  
   {
     printf("%lu ", a[start]);
@@ -38,17 +42,19 @@ int binarySearch(keytype* a, int mid, int r_start, int r_end)
 }
 */
 
+/*Serial Merge Routine*/
 keytype* smerge(keytype *arr, int l_start, int l_end, int r_start, int r_end)
 {
 
   int l_len = l_end - l_start + 1;
   int r_len = r_end - r_start;
-   
+
+#ifdef DEBUG   
   printf("Full Array : \n"); display(arr, 0, r_len+l_len); 
   printf("Left Array : \n"); display(arr, 0, l_len);
   printf("Right Array : \n"); display(arr, r_start, r_len);
   printf("l_start  %3d l_end %3d  r_start %3d r_end %3d l_len %3d r_len %3d\n", l_start, l_end, r_start, r_end, l_len, r_len);
-
+#endif
   int i=0;
   
   keytype *newArr = (keytype*) malloc((r_len + l_len)*sizeof(keytype));
@@ -68,8 +74,11 @@ keytype* smerge(keytype *arr, int l_start, int l_end, int r_start, int r_end)
     	r_start++;
     }
   }
-  
+
+#ifdef DEBUG  
   printf("l_start  %3d l_end %3d  r_start %3d r_end %3d l_len %3d r_len %3d\n", l_start, l_end, r_start, r_end, l_len, r_len);
+#endif
+
   while(l_start <= l_end)
   {
     newArr[i] = arr[l_start];
@@ -125,8 +134,6 @@ keytype* pmerge(keytype *arr, int l_start, int l_end, int r_start, int r_end)
 void mergeSort(int start, int end, keytype* a)
 {
   int N = end - start;
-
-  printf("Hey this is N    : %3d\n", N);
 
   const int G = 1000; 
   if(N <= G)
